fa6d35e3bdcc3f1495c7b56a2adeebee630faa5e
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-05-22 13:23:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+IAmazonS3 client = new AmazonS3Client();
+
+var request = new PutBucketRequest
+{
+    BucketName = "luiscocoenriquezbucket",
+    UseClientRegion = true,
+};
+
+var response = await client.PutBucketAsync(request);
+
